27f42baa7d9a333644add18814a1ef8fab29419b
Eugène Villotte <eugene.villotte@gmail.com>
2023-07-07 13:09:08 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <format>
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << std::format("-- MathLexer --\nYou must specify a math expression.\n\n  Usage:   {} <MathExpression>\n  Example: {} '3*2+4*9'\n", argv[0], argv[0]);
+    return 1;
+  }
+  std::string rawExpr = argv[1];
+  std::cout << std::format(">>> Raw \"{}\" -> length: {}", rawExpr, rawExpr.length()) << std::endl;
+  return 0;
+}
