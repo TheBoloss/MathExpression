dd5c59cc3e0aff9d219c3e6d251cb60e309d01b6
Eugène Villotte <eugene.villotte@gmail.com>
2023-07-07 13:43:22 +0200

    feat: change project name to MathExpression

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
   if (argc != 2)
   {
-    std::cerr << std::format("-- MathLexer --\nYou must specify a math expression.\n\n  Usage:   {} <MathExpression>\n  Example: {} '3*2+4*9'\n", argv[0], argv[0]);
+    std::cerr << std::format("-- MathExpression --\nYou must specify a math expression.\n\n  Usage:   {} <MathExpression>\n  Example: {} '3*2+4*9'\n", argv[0], argv[0]);
     return 1;
   }
   std::string rawExpr = argv[1];
